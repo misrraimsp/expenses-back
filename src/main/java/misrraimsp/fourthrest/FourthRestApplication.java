1f089822f7c3274bb04ae7a33a9a49328059dfdc
Misrraim <misrraimsp@gmail.com>
2020-10-10 13:27:01 +0100

    initial commit

---
+++ src/main/java/misrraimsp/fourthrest/FourthRestApplication.java
@@ -0,0 +1,13 @@
+package misrraimsp.fourthrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourthRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/misrraimsp/fourthrest/FourthRestApplicationTests.java
@@ -0,0 +1,13 @@
+package misrraimsp.fourthrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
